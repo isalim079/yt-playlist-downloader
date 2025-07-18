ed6fea0dbed929b060813938dd833933e585956f
isalim079 <salimreza1025@gmail.com>
2025-07-18 15:43:18 +0600

    Vite pwa installed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate", // optional: auto update service worker
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"], // optional
+      manifest: {
+        name: "My Vite PWA",
+        short_name: "VitePWA",
+        description: "My Progressive Web App built with Vite and React",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
